5954a0555b463c08dbd0ee4628ba7276dff40593
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2022-02-13 23:46:51 +0530

    initiated buffer formation

---
+++ buffer.c
@@ -0,0 +1,11 @@
+#include "keywordMap.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 4096    // buffer size in bytes
+
+char** init_buffer(FILE* fp){
+    char buffer[2*BUFFER_SIZE]; 
+
+    return buffer;
+}
